1dac766ba64c88f08f70713054c39621a30fe0fa
Prosper Nnadi <prospernnadi3@gmail.com>
2021-09-26 23:45:21 +0100

    Edited js file

--- battleship.js
+++ battleship.js
@@ -1,14 +1,14 @@
 var view = {
-    displayMessage: function(msg) {
+  displayMessage: function (msg) {
     var messageArea = document.getElementById("messageArea");
     messageArea.innerHTML = msg;
-    },
-    displayHit: function(location) {
+  },
+  displayHit: function (location) {
     var cell = document.getElementById(location);
     cell.setAttribute("class", "hit");
-    },
-    displayMiss: function(location) {
+  },
+  displayMiss: function (location) {
     var cell = document.getElementById(location);
     cell.setAttribute("class", "miss");
-    }
-    };
+  },
+};
